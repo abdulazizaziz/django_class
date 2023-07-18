7db7261cd8efd39f9cdbc8035fc9110d694ef159
unknown <abdulazizazizarab@gmail.com>
2023-07-18 06:05:11 -0700

    18 June

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from products.views import home, products
+from products.views import home, products, create_product, delete_product
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
     path('products/', products),
+    path('createproducts/', create_product),
+    path('deleteproducts/', delete_product),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 from .models import Product
+
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def home(request):
@@ -7,8 +10,8 @@ def home(request):
 
 
 def products(request):
-    # products = Product.objects.all()
-    products = Product.objects.filter(is_available=True)
+    products = Product.objects.all()
+    # products = Product.objects.filter(is_available=True)
     context = {
         "count": len(products),
         'products': products
@@ -19,3 +22,28 @@ def products(request):
     # print(iphone.name)
 
     return render(request, 'products.html', context)
+    
+def create_product(request):
+    # print(request.method)
+    # print(request.GET)
+    # print(dict(request.GET))
+    # print(request.POST)
+    name = request.POST['name']
+    price = request.POST['price']
+    detail = request.POST['detail']
+    if 'is_available' in request.POST:
+        is_available = True
+    else:
+        is_available = False
+    
+    Product.objects.create(name=name, price=price, detail=detail, is_available=is_available)
+
+    return HttpResponseRedirect('/products')
+
+def delete_product(request):
+    try:
+        item = Product.objects.get(id=request.GET['id'])
+        item.delete()
+    except ObjectDoesNotExist:
+        pass
+    return HttpResponseRedirect('/products')
